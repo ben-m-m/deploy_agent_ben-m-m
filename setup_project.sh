dd2b7f1e53d889d5e3252eb2110033d08de8985c
ben-m-m <mainaben23@gmail.com>
2026-02-12 23:47:30 +0200

    checks dir before creating and proceeds to threshold edit.

--- setup_project.sh
+++ setup_project.sh
@@ -4,6 +4,8 @@ echo "Create parent directory attendance_tracker_{your name/version}."
 read -p "Enter directory suffix?:" verdir
 att_dir="attendance_tracker_$verdir"
 
+
+#function for clean up followed by trap upon sigint
 clean_up() {
 	echo "Interupted process, closing now:"
 	tar -czvf "${att_dir}_archive.tar.gz" "$att_dir"
@@ -12,21 +14,23 @@ clean_up() {
 
 trap clean_up SIGINT
 
-mkdir -p "$att_dir"
-touch "$att_dir/attendance_checker.py"
-mkdir -p "$att_dir/Helpers/"
-touch "$att_dir/Helpers/assets.csv"
-touch "$att_dir/Helpers/config.json"
-mkdir -p "$att_dir/reports"
-touch "$att_dir/reports/reports.log"
 
-cat > "$att_dir/Helpers/config.json" <<EOF
-{
-  "warning": 75,
-  "failure": 50
-}
-EOF
+#Check if directory exists and if not, creates directory
+if [ -d "$att_dir" ]; then
+	echo ""
+	echo "Directory and files already exist"
+	echo "----------------------------------"
+else
+	mkdir -p "$att_dir"
+	touch "$att_dir/attendance_checker.py"
+	mkdir -p "$att_dir/Helpers/"
+	touch "$att_dir/Helpers/assets.csv"
+	touch "$att_dir/Helpers/config.json"
+	mkdir -p "$att_dir/reports"
+	touch "$att_dir/reports/reports.log"
+fi
 
+#Prompts the user to change the attendance threshold
 
 read -p "Do you want to update the attendance threshhold ? (y/n)" choice
 if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
@@ -34,5 +38,7 @@ if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
     read -p "Enter failure threshhold (default 50): " failure
 
     sed -i "s/\"warning\": [0-9]\+/\"warning\": $warning/" "$att_dir/Helpers/config.json"
-    sed -i "s/\"default\": [0-9]\+/\"default\": $failure/" "$att_dir/Helpers/config.json"
+    sed -i "s/\"failure\": [0-9]\+/\"failure\": $failure/" "$att_dir/Helpers/config.json"
 fi
+
+
