bdd19190afa305004d70b3a97dcb82a5e915df32
ben-m-m <mainaben23@gmail.com>
2026-02-18 13:15:08 +0200

    Add initial setup for attendance tracking with configuration validation and user prompts for threshold updates

--- setup_project.sh
+++ setup_project.sh
@@ -30,12 +30,58 @@ else
 	touch "$att_dir/reports/reports.log"
 fi
 
+#verify the json file exists and if not, creates it with default values
+if [[ ! -s "$att_dir/Helpers/config.json" ]]; then
+cat > "$att_dir/Helpers/config.json" <<EOF
+{
+  "warning": 75,
+  "failure": 50
+}
+EOF
+fi
+
+ #warning=${warning:-75}
+ #failure=${failure:-50}
+
+
+
 #Prompts the user to change the attendance threshold
 
 read -p "Do you want to update the attendance threshhold ? (y/n)" choice
 if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
     read -p "Enter warning threshhold (default 75): " warning
     read -p "Enter failure threshhold (default 50): " failure
+
+    #default value incase User clicks enter key
+    warning=${warning:-75}
+    failure=${failure:-50}
+
+    #verify user input as numeric value
+    
+    if ! [[ "$warning" =~ ^[0-9]+$ ]] || ! [[ "$failure" =~ ^[0-9]+$ ]]; then
+	    echo ""
+	    echo "-------------------------------------"
+	    echo "Thresholds must be numeric values !!!"
+	    exit 1
+    fi
+
+    
+	#check that the values are non negative but also less than 100
+
+	if [[ "$warning" -le 0 || "$warning" -gt 100 || "$failure" -le 0 || "$failure" -gt 100 ]]; then
+		echo ""
+		echo "Thresholds must be between 1 and 100 !!!"
+		exit 1
+	fi
+
+	#Correct entries where warning must be higher than failure
+    
+    if [[ "$warning" -le "$failure" ]]; then
+	    echo ""
+	    echo "Warning threshold must be higher than failure threshold !!!"
+	    exit 1
+    fi
+
 
     sed -i "s/\"warning\": [0-9]\+/\"warning\": $warning/" "$att_dir/Helpers/config.json"
     sed -i "s/\"failure\": [0-9]\+/\"failure\": $failure/" "$att_dir/Helpers/config.json"
